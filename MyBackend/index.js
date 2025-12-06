64c8eca2660a698a9be116da26a0483ffc47c983
LAKSHMINARAYANA <lakshminarayana.makineedi@gmail.com>
2025-12-06 11:38:10 +0530

    Updated files

---
+++ MyBackend/index.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const app=express();
+app.use(express.json());
+app.get("/",(req,res)=>{
+    res.send("Backend is running");
+});
+app.post('/add-user',(req,res)=>{
+    let data=req.body;
+    console.log(data);
+    res.send(`Added data`);
+})
+app.put('/edit-user',(req,res)=>{
+    let data=req.body;
+    console.log(data);
+    res.send(`edited data`);
+})
+app.delete('/delete-user',(req,res)=>{
+    let data=req.body;
+    console.log(data);
+    res.send(`deleted data`);
+})
+app.listen(3000,()=>{
+    console.log("server started on port 3000");
+});
